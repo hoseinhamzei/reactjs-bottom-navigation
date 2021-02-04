746c8e97b7d6627f4f3365003442f9aa65f183e8
unknown <hnkn1998@gmail.com>
2021-02-04 18:50:00 +0330

    Updates

---
+++ precache-manifest.892ddc30fd6a2752517bb2d364e404c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459048b9b80b98f4d9257f05e3416489",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee8578eeb1bfd3d93819",
+    "url": "./static/css/main.52b38bf0.chunk.css"
+  },
+  {
+    "revision": "6f2cff49341292e022d1",
+    "url": "./static/js/2.39746aea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.39746aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8578eeb1bfd3d93819",
+    "url": "./static/js/main.f3d54ad4.chunk.js"
+  },
+  {
+    "revision": "d777421d63304cee5bec",
+    "url": "./static/js/runtime-main.523e4ce4.js"
+  }
+]);

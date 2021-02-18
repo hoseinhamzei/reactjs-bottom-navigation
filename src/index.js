299afa61e539bcadb13434d5bb76b051347a2ba4
unknown <hnkn1998@gmail.com>
2021-02-18 19:06:10 +0330

    added custom active bg color and active text color support

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React, { useState } from 'react'
 
 const BottomNavigation = ({
   items,
-  defaultSelectedTab = null,
+  defaultSelected = null,
   onItemClick,
-  noActiveBg
+  noActiveBg,
+  activeBgColor,
+  activeTextColor
 }) => {
-  const [current, setCurrent] = useState(defaultSelectedTab)
+  const [current, setCurrent] = useState(defaultSelected)
+
+  function getItemStyle() {
+    const style = {}
+    if (noActiveBg && !activeBgColor) {
+      style.backgroundColor = 'transparent'
+    }
+    if (activeBgColor) {
+      style.backgroundColor = activeBgColor
+    }
+    return style
+  }
 
   const navItems = items.map((item, idx) => {
     return (
@@ -14,7 +27,7 @@ const BottomNavigation = ({
         key={'nav-item-' + idx}
         id={'nav-item-' + idx}
         className={`bottom-nav-item ${current === idx && 'active'}`}
-        style={noActiveBg && { backgroundColor: 'transparent' }}
+        style={current === idx ? getItemStyle() : {}}
         onClick={() => {
           setCurrent(idx)
           if (item.onClick) {
@@ -27,7 +40,14 @@ const BottomNavigation = ({
       >
         {current !== idx && item.icon ? item.icon : ''}
         {current === idx && item.activeIcon ? item.activeIcon : ''}
-        {item.title && <p className='bottom-nav-item--title'>{item.title}</p>}
+        {item.title && (
+          <p
+            className='bottom-nav-item--title'
+            style={current === idx ? { color: activeTextColor } : {}}
+          >
+            {item.title}
+          </p>
+        )}
       </div>
     )
   })
